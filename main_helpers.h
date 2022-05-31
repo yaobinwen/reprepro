b435539d1c460752d0971f6abc2c2e7cf427ab41
Yaobin Wen <robin.wyb@gmail.com>
2022-05-31 19:02:04 -0400

    Extract code into `main_helpers.c`

--- main_helpers.h
+++ main_helpers.h
@@ -1,6 +1,14 @@
 #ifndef REPREPRO_MAIN_HELPERS_H
 #define REPREPRO_MAIN_HELPERS_H
 
+#include <stdbool.h>
+
 void myexit(int) __attribute__((__noreturn__));
+
+void disallow_plus_prefix(const char *dir, const char *name, const char *allowed);
+
+char *expand_plus_prefix(/*@only@*/char *dir, const char *name, const char *allowed, bool freedir);
+
+int callendhook(int status, char *argv[]);
 
 #endif
